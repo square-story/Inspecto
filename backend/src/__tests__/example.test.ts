148276c0bb6239326013466975fa3ec60e3269c3
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2025-01-07 20:39:09 +0530

    add initial backend server setup and sample test cases

---
+++ backend/src/__tests__/example.test.ts
@@ -0,0 +1,8 @@
+// src/__tests__/example.test.ts
+import { describe, it, expect } from "@jest/globals";
+
+describe("Sample Test", () => {
+    it("should pass", () => {
+        expect(true).toBe(true);
+    });
+});
