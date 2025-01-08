b094d54011d562d3e971686c9aad67b440dd4a41
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2025-01-08 09:22:59 +0530

    implement user repository methods for create, find, update, delete, and retrieve all users

--- backend/src/repositories/user.repository.ts
+++ backend/src/repositories/user.repository.ts
@@ -1,8 +1,24 @@
-import Users from '../models/user.model'
+import Users, { IUsers } from '../models/user.model';
+import { IUserRepository } from './interfaces/user.repository.interface';
 
-export class UserRepository {
-    //Create a New User
-    async createUser(userDate: any) {
-        const newUser = new Users(userDate)
+
+class UserRepository implements IUserRepository {
+    async createUser(userData: IUsers): Promise<IUsers> {
+        const user = new Users(userData);
+        return await user.save();
+    }
+    async findUserByEmail(email: string): Promise<IUsers | null> {
+        return await Users.findOne({ Email: email });
+    }
+    async updateUser(userId: string, updates: Partial<IUsers>): Promise<IUsers | null> {
+        return await Users.findByIdAndUpdate(userId, updates, { new: true })
+    }
+    async deleteUser(userId: string): Promise<IUsers | null> {
+        return await Users.findByIdAndDelete(userId);
+    }
+    async getAllUsers(): Promise<IUsers[]> {
+        return await Users.find();
     }
 }
+
+export default new UserRepository()
