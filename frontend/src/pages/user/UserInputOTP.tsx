504ba1186b28914b8805c5ebfebd60d7867420a0
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2025-01-16 09:12:37 +0530

    feat: remove unused LoginPreview component; streamline user authentication flow

--- frontend/src/pages/user/UserInputOTP.tsx
+++ frontend/src/pages/user/UserInputOTP.tsx
@@ -4,9 +4,7 @@ import OTPComponent from "@/components/OTPComponent"
 const UserInputOTP = () => {
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-
             <div className="w-full max-w-sm">
-
                 <OTPComponent />
             </div>
         </div>
