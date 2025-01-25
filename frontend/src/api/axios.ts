19af910aab941ca96d8f74279a019db52cccf9db
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2025-01-25 10:16:53 +0530

    feat: restructure Redux store setup; move store configuration to a new index file and update import paths

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { store } from '../features/store';
+import { store } from '@/store';
 import { clearCredentials, setCredentials } from '../features/auth/authSlice';
 
 const axiosInstance = axios.create({
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from "react-redux";
 import './index.css'
 import App from './App.tsx'
-import { store } from './features/store.ts';
+import { store } from '@/store';
 import { ThemeProvider } from "./components/ui/theme-provider.tsx"
 import { Toaster } from 'sonner';
 
