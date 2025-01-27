68f6332085d0cc80be374e36c2cde7c2a124a956
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2025-01-27 15:46:47 +0530

    feat: implement Inspector Management page with data table and actions

---
+++ frontend/src/app/InspectorManagment/columns.tsx
@@ -0,0 +1,63 @@
+//contain our column definitions.
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export type Inspectors = {
+    id: string;
+    firstName: string;
+    email: string;
+    status: 'blocked' | 'unblock' | 'pending'
+}
+
+export const columns: ColumnDef<Inspectors>[] = [
+    {
+        accessorKey: "status",
+        header: "Status",
+    },
+    {
+        accessorKey: "firstName",
+        header: "FirstName"
+    },
+    {
+        accessorKey: "email",
+        header: "Email"
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const payment = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() => navigator.clipboard.writeText(payment.id)}
+                        >
+                            Copy payment ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View customer</DropdownMenuItem>
+                        <DropdownMenuItem>View payment details</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
+    },
+]
---
+++ frontend/src/app/InspectorManagment/page.tsx
@@ -0,0 +1,48 @@
+// app/InspectorManagement/DemoPage.tsx
+"use client";
+
+import { useEffect, useState } from "react";
+import { DataTable } from "./data-table";
+import { Inspectors, columns } from "./columns";
+
+export default function DemoPage() {
+    const [data, setData] = useState<Inspectors[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await getData();
+            setData(response);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div className="container mx-auto py-10">
+            <DataTable columns={columns} data={data} />
+        </div>
+    );
+}
+
+async function getData(): Promise<Inspectors[]> {
+    return [
+        {
+            id: "728ed52f",
+            firstName: "sadik",
+            email: "sadik@gmail.com",
+            status: "pending",
+        },
+        {
+            id: "489e1d42",
+            firstName: "somethings",
+            status: "blocked",
+            email: "example@gmail.com",
+        },
+        {
+            id: "2385340",
+            firstName: "sfdoome",
+            status: "unblock",
+            email: "some@gmail.com",
+        },
+
+    ];
+}
