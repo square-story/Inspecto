748e0f96e3c462cfe43ae33f3bb4342af92b3d4b
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2025-01-27 12:04:01 +0530

    feat: refactor InspectorDashboard layout to enhance navigation and user interface components

--- frontend/src/pages/inspector/InspectorDashboard.tsx
+++ frontend/src/pages/inspector/InspectorDashboard.tsx
@@ -1,12 +1,24 @@
 import NavBar from "@/app/InspectorDashBoard/layout/InspectorNavBar"
 import Layout from "@/app/InspectorDashBoard/layout"
 import { Outlet } from "react-router-dom"
+import { Header } from "@/app/adminDashboard/layout/Header"
+import { ModeToggle } from "@/components/ui/DarkModeSwitch"
+import { UserNav } from "@/app/InspectorDashBoard/components/UserNav"
+import { Search } from "@/app/InspectorDashBoard/components/Search"
 
 const InspectorDashboard = () => {
     return (
         <>
             <Layout>
-                <NavBar />
+                <Header>
+                    <NavBar />
+                    <h1 className="font-bold text-xl">inspecto</h1>
+                    <div className='ml-auto flex items-center gap-4'>
+                        <Search />
+                        <ModeToggle />
+                        <UserNav />
+                    </div>
+                </Header>
                 <Outlet />
             </Layout >
         </>
