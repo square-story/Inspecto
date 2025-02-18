ab9c37eacb4939649cae279129a3898db6612f3f
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2025-02-18 10:38:32 +0530

    feat: create richColors into toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <App />
-        <Toaster />
+        <Toaster richColors />
       </ThemeProvider>
     </Provider>
   </ConfirmDialogProvider>
